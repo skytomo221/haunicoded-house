906a63ee6228ac5a64c1f54e9a21e4c3009db386
skytomo <tomo1kawa.fullerene@gmail.com>
2021-10-29 09:22:59 +0900

    Add title logo

--- src/scenes/title-scene.ts
+++ src/scenes/title-scene.ts
@@ -8,26 +8,46 @@ export default class TitleScene extends Phaser.Scene {
     });
   }
 
+  preload() {
+    this.load.svg('title', '../../asset/images/title.svg');
+  }
+
   create(): void {
+    this.setTitle();
     this.add.existing(
-      new TextButton(this, 100, 110, 'EASY', {}, () => {
+      new TextButton(this, 100, 200, 'EASY', {}, () => {
         this.scene.start('GameScene', { timelimit: 4 ** 5 * 1000 });
       }),
     );
     this.add.existing(
-      new TextButton(this, 100, 130, 'NORMAL', {}, () => {
+      new TextButton(this, 100, 220, 'NORMAL', {}, () => {
         this.scene.start('GameScene', { timelimit: 4 ** 4 * 1000 });
       }),
     );
     this.add.existing(
-      new TextButton(this, 100, 150, 'HARD', {}, () => {
+      new TextButton(this, 100, 240, 'HARD', {}, () => {
         this.scene.start('GameScene', { timelimit: 4 ** 3 * 1000 });
       }),
     );
     this.add.existing(
-      new TextButton(this, 100, 170, 'VERY HARD', {}, () => {
+      new TextButton(this, 100, 260, 'VERY HARD', {}, () => {
         this.scene.start('GameScene', { timelimit: 4 ** 2 * 1000 });
       }),
     );
+  }
+
+  private setTitle() {
+    const title = this.add.image(0, 0, 'title');
+
+    const scaleX = this.cameras.main.width / title.width / 4;
+    const scaleY = this.cameras.main.height / title.height / 4;
+    const scale = Math.max(scaleX, scaleY);
+    title
+      .setScale(scale)
+      .setPosition(
+        this.cameras.main.width / 2 - title.getCenter().x / 2,
+        this.cameras.main.height / 5,
+      )
+      .setScrollFactor(0);
   }
 }
